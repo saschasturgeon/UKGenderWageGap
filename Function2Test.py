021b8edd326d86dddd375660f23572b1ab7409f5
saschasturgeon <georgia.spener@gmail.com>
2024-12-25 17:30:19 +0000

    trial function added to check circleci connection

--- Function2Test.py
+++ Function2Test.py
@@ -1,1 +1,12 @@
+def isnumber(x):
+    try:
+        int(x)
+        if(x<0):
+            return("Negative")
+        elif(x>0):
+            return("Positive")
+        else:
+            return("Neither")
+    except ValueError:
+        return("Not a number")
 
