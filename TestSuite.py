ae0e4881b221ae368a74e4b6c43d74dfa2eece1c
saschasturgeon <georgia.spener@gmail.com>
2024-12-25 17:31:49 +0000

    trial unit tests added to check circle ci connection

--- TestSuite.py
+++ TestSuite.py
@@ -1,1 +1,22 @@
+import unittest
+from FunctionToTest import isnumber
 
+class TestFunction(unittest.TestCase):
+    def test_positive_number(self):
+        result = isnumber(4)
+        self.assertEqual(result, "Positive")
+
+    def test_negative_number(self):
+        result = isnumber(-14)
+        self.assertEqual(result, "Negative")
+    
+    def test_zero(self):
+        result = isnumber(0)
+        self.assertEqual(result, "Neither")
+    
+    def test_is_a_number(self):
+        result = isnumber("fhweuh")
+        self.assertEqual(result,"Not a number")
+
+if __name__ == '__main__':
+    unittest.main()
