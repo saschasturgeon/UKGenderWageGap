5da11fb36d84f86266baf31753ba992f7126466f
Sascha Sturgeon <georgia.spener@gmail.com>
2025-01-05 18:23:22 +0000

    Corrected the file names and added a 2nd unit test to check the correct slope and intercept were  being calculated.

--- test_Suite.py
+++ test_Suite.py
@@ -1,15 +1,16 @@
 import unittest
+import numpy as np
 from Function2Test import fitline
 
 class TestFunction(unittest.TestCase):
 
+    def test_stats(self):
+        result = fitline('gender-wage-gap-UK.csv')
+        self.assertEqual(result,(np.float64(-0.6130347593582702), np.float64(1251.796791443812)))
+
     def test_filetype(self):
         result = fitline('gender-wage-gap-UK.numbers')
         self.assertEqual(result, "Please enter the correct file type and try again")
-
-    def check_stats(self):
-        result = fitline('gender-wage-gap-UK.csv')
-        self.assertEqual(result,"-0.6130347593582889, 1251.7967914438502")
 
 if __name__ == '__main__':
     unittest.main()
