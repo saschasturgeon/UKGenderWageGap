3662b8c85e89d6407fa44caf3645582d43381a31
Sascha Sturgeon <georgia.spener@gmail.com>
2025-01-05 17:40:45 +0000

    Updates to the function to test

--- Function2Test.py
+++ Function2Test.py
@@ -1,9 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 def fitline(filepath):
     if filepath.endswith(".csv"):
-        data = pd.read_csv(r"filepath")
-        x = data[0]
-        y = data[1]
-        plt.plot(x,y)
+        data = pd.read_csv(filepath)
+        x = data.iloc[:,0]
+        y = data.iloc[:,1]
+        plt.scatter(x,y)
         slope, intercept = np.polyfit(x,y,1)
         plt.plot(x, slope*x+intercept, color = 'red')
         return(slope,intercept)
--- TestSuite.py
+++ TestSuite.py
@@ -5,6 +5,8 @@ class TestFunction(unittest.TestCase):
     def test_filetype(self):
         result = fitline('gender-wage-gap-UK.numbers')
         self.assertEqual(result, "Please enter the correct file type and try again")
+    def check_stats(self):
+        result = fitline('gender-wage-gap-UK.csv')
 
 if __name__ == '__main__':
     unittest.main()
