44f663b0b5b4ec9fea4525aff483db6dc27e807c
Sascha Sturgeon <georgia.spener@gmail.com>
2025-01-05 18:11:37 +0000

    activated virtual environment in circleci

--- TestSuite.py
+++ TestSuite.py
@@ -2,11 +2,14 @@ import unittest
 from Function2Test import fitline
 
 class TestFunction(unittest.TestCase):
+
     def test_filetype(self):
         result = fitline('gender-wage-gap-UK.numbers')
         self.assertEqual(result, "Please enter the correct file type and try again")
+
     def check_stats(self):
         result = fitline('gender-wage-gap-UK.csv')
+        self.assertEqual(result,"-0.6130347593582889, 1251.7967914438502")
 
 if __name__ == '__main__':
     unittest.main()
