15d2a81f6e9debed22d3ad525942ba0a1e481512
Sascha Sturgeon <georgia.spener@gmail.com>
2025-01-10 00:37:41 +0000

    added the unit test to check the intercept

--- test_Suite.py
+++ test_Suite.py
@@ -4,10 +4,15 @@ from Function2Test import fitline
 
 class TestFunction(unittest.TestCase):
 
-    def test_stats(self):
+    def test_slope(self):
         result = fitline('gender-wage-gap-UK.csv')
         slope = result[0]
         self.assertAlmostEqual(slope,-0.613, 2)
+    
+    def test_intercept(self):
+        result = fitline('gender-wage-gap-UK.csv')
+        intercept = result[1]
+        self.assertAlmostEqual(intercept,1251.80, 2)
 
     def test_filetype(self):
         result = fitline('gender-wage-gap-UK.numbers')
